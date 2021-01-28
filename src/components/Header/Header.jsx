a92cd7989607845d731348a9a40ea78276add373
Blossom Babalola <sapashb@gmail.com>
2021-01-28 16:31:33 +0100

    update layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header>
+      <div></div>
+      <ul>
+        <li>home</li>
+        <li>Find A Buddy</li>
+        <li>ChatRoom</li>
+        <li>Message</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/Header/Header"
+import Layout from "../components/layout"
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      <Header />
+      <h1>Hello world!</h1>
+    </Layout>
+  )
+}
+
+export default Dashboard
